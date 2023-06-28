78880f7f1886cd8859671847fa86843ea90868a2
amareLD <80774058+amareLD@users.noreply.github.com>
2023-06-28 20:04:30 +0530

    Create-React-Context

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,8 +6,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 //import { LinkContainer } from 'react-router-bootstrap/LinkContainer';
+import { useContext } from 'react';
+import { Store } from './Store';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -29,6 +36,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Attire Avenue</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
